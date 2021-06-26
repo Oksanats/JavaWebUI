4b7d479f6bc7ce15f668e80784a50dbae8ad4cf3
Oksanats <oksa.romanowa@gmail.com>
2021-06-26 20:33:33 +0300

    Initial commit

---
+++ src/main/java/com/geekbrainsstart/App.java
@@ -0,0 +1,22 @@
+package com.geekbrainsstart;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+
+    public static int sum(int a,int b){
+       return a + b;
+
+    }
+
+    public static int simpleDiv(int a,int b){
+        return b == 0 ? -1: a/b;
+    }
+}
